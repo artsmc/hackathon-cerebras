a1ca9261ab51dc57bc9c757690aa2b33091b7823
vik <vikram.sharma1024@gmail.com>
2025-08-02 20:33:56 -0400

    login page

--- policyglass/src/app/login/page.tsx
+++ policyglass/src/app/login/page.tsx
@@ -8,19 +8,28 @@ export default async function LoginPage() {
   
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+    <div 
+      className="font-sans min-h-screen py-4 pb-20 relative"
+      style={{ 
+        backgroundImage: "url('/login.jpg')",
+        backgroundSize: '112.5%',
+        backgroundPosition: '35% center',
+        backgroundRepeat: 'no-repeat'
+      }}
+    >
+      <main className="absolute top-1/2 left-[10%] transform -translate-y-1/2 w-full max-w-md">
+        
+        <div className="w-full">
+          <h1 className="text-3xl font-bold tracking-tight text-white mb-8 text-shadow-lg/20">
           Login to PolicyGlass
         </h1>
         
-        <div className="w-full max-w-md">
           <LoginForm />
           
           <div className="mt-4 text-center">
-            <p className="text-foreground">
+            <p className="text-background">
               Don't have an account?{' '}
-              <a href="/register" className="text-blue-500 hover:underline">
+              <a href="/register" className="text-lime-300 hover:underline">
                 Register here
               </a>
             </p>
