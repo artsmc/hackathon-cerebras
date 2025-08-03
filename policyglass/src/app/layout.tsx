489165dfa3e635fe1e386bdf8dcb637ed5b539df
Mark Campbell <artsmc88@gmail.com>
2025-08-03 09:49:44 -0400

    fixed layout

--- policyglass/src/app/layout.tsx
+++ policyglass/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
-import { unstable_ViewTransition as ViewTransition } from 'react';
 import { ReactQueryProvider } from './components/ReactQueryProvider';
 
 const geistSans = Geist({
@@ -30,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ReactQueryProvider>
-          <ViewTransition>{children}</ViewTransition>
+          {children}
         </ReactQueryProvider>
       </body>
     </html>
